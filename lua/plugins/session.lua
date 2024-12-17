d43a87fdf713a5f76f3e1f21c9a3dc8d3ab6ed9f
Igor Gorohovsky <igor.gorohhovsky@yandex.ru>
2024-12-17 20:53:19 +0200

    session manager expirience enhancements

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -5,7 +5,7 @@ return {
 			local config = require("session_manager.config")
 			opts = opts or {}
 			local updated_opts = {
-				autoload_mode = { config.AutoloadMode.CurrentDir, config.AutoloadMode.LastSession },
+				autoload_mode = { config.AutoloadMode.GitSession, config.AutoloadMode.LastSession },
 			}
 
 			for k, v in pairs(updated_opts) do
