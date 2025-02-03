1907b1453e22d1e2c66855dc81bf3cd5c9a94fff
Igor Gorohovsky <igor.gorohhovsky@yandex.ru>
2025-02-03 15:31:13 +0200

    remove hop, noice and add lualine

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -30,19 +30,3 @@ map("n", "<leader>fd", builtin.diagnostics)
 
 -- LSP symbol rename
 map("n", "<leader>lr", vim.lsp.buf.rename)
-
--- Hop
-local hop = require("hop")
-local directions = require("hop.hint").HintDirection
-map("", "f", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-end, { remap = true })
-map("", "F", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-end, { remap = true })
-map("", "t", function()
-	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-end, { remap = true })
-map("", "T", function()
-	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-end, { remap = true })
