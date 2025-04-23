ca49193bce308698540ef90a6737f4ebbede250e
Igor Gorohovsky <igor.gorohhovsky@yandex.ru>
2025-04-23 20:23:35 +0300

    feat: add harpoon plugin

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,7 +14,7 @@ vim.cmd([[cab wrap set wrap! \| set linebreak!]])
 local builtin = require("telescope.builtin")
 local utils = require("utils.telescope")
 
-map("n", "<leader><leader>", utils.find_files)
+map("n", "<leader>ff", utils.find_files)
 map("n", "<leader>fb", builtin.buffers)
 map("n", "<leader>fw", builtin.live_grep)
 map("n", "<leader>fh", builtin.highlights)
@@ -29,5 +29,28 @@ map("n", "gr", builtin.lsp_references)
 map("n", "gd", builtin.lsp_definitions)
 map("n", "<leader>fd", builtin.diagnostics)
 
+-- Harpoon mappings
+local harpoon = require("harpoon")
+map("n", "mm", function()
+	harpoon:list():add()
+end)
+map("n", "<leader><leader>", function()
+	harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+harpoon:extend({
+	UI_CREATE = function(cx)
+		vim.keymap.set("n", "<C-v>", function()
+			harpoon.ui:select_menu_item({ vsplit = true })
+		end, { buffer = cx.bufnr })
+
+		vim.keymap.set("n", "<C-x>", function()
+			harpoon.ui:select_menu_item({ split = true })
+		end, { buffer = cx.bufnr })
+
+		vim.keymap.set("n", "<C-t>", function()
+			harpoon.ui:select_menu_item({ tabedit = true })
+		end, { buffer = cx.bufnr })
+	end,
+})
 -- LSP symbol rename
 map("n", "<leader>lr", vim.lsp.buf.rename)
--- lua/utils/telescope.lua
+++ lua/utils/telescope.lua
@@ -1,6 +1,25 @@
 local M = {}
 local builtin = require("telescope.builtin")
 
+local conf = require("telescope.config").values
+M.list_harpoon_marks = function(harpoon_files)
+	local file_paths = {}
+	for _, item in ipairs(harpoon_files.items) do
+		table.insert(file_paths, item.value)
+	end
+
+	require("telescope.pickers")
+		.new({}, {
+			prompt_title = "Harpoon",
+			finder = require("telescope.finders").new_table({
+				results = file_paths,
+			}),
+			previewer = conf.file_previewer({}),
+			sorter = conf.generic_sorter({}),
+		})
+		:find()
+end
+
 M.find_files = function(opts)
 	return builtin.find_files({ hidden = true, no_ignore = true })
 end
