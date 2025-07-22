33adad5c2a19f1193d89814db9b8cd619350f630
Igor Gorohovsky <igor.gorohhovsky@yandex.ru>
2025-07-22 18:21:35 +0300

    ref: default opts for oil

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -4,7 +4,10 @@ return {
 		"stevearc/oil.nvim",
 		---@module 'oil'
 		---@type oil.SetupOpts
-		opts = {},
+		opts = {
+            default_file_explorer = true,
+            use_default_keymaps = true,
+        },
 		dependencies = { { "echasnovski/mini.icons", opts = {} } },
 		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
